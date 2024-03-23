b2aae0ebb6443dee72fa9ea83344679ab0f397b9
bipinmsit <bipinprv@gmail.com>
2024-03-23 21:03:20 +0530

    modifying the code

--- option_chain.py
+++ option_chain.py
@@ -4,14 +4,25 @@ import pandas as pd
 import xlwings as xw
 import time
 
+index_options = ["NIFTY", "BANKNIFTY", "FINNIFTY", "MIDCPNIFTY"]
+
+# Parameters
 sym = "NIFTY"
 exp_date = "28-Mar-2024"
+interval = 3  # In seconds
+next_row = 5  # PCR Column Values
+call_oi_chg_column = "K2"
+put_oi_chg_column = "K3"
 
 file = xw.Book(
     r"C:\Users\bipin\OneDrive\Desktop\bipinmsit\tradetools\option_chain.xlsx"
 )
-sh1 = file.sheets("Sheet1")
-sh2 = file.sheets("Sheet2")
+sh1 = file.sheets("option_chain")
+sh2 = file.sheets("expiry_date")
+
+# Column range to clear
+column_to_clear = "A"
+column_range = sh1.range("A:H")
 
 
 def oc(symbol, expiry_date):
@@ -53,21 +64,63 @@ def oc(symbol, expiry_date):
 
     df = pd.concat([ce_df, pe_df], axis=1)
 
-    return expiry_list, df
+    # Selected DataFrame
+    df_selected = df[
+        [
+            "lastPrice_CE",
+            "openInterest_CE",
+            "changeinOpenInterest_CE",
+            "strikePrice_CE",
+            "changeinOpenInterest_PE",
+            "openInterest_PE",
+            "lastPrice_PE",
+        ]
+    ]
+
+    final_df = df_selected.rename(
+        columns={
+            "lastPrice_CE": "Call_LTP",
+            "openInterest_CE": "Call_OI",
+            "changeinOpenInterest_CE": "OI_Chg",
+            "strikePrice_CE": "Strike",
+            "changeinOpenInterest_PE": "OI_Chg",
+            "openInterest_PE": "Put_OI",
+            "lastPrice_PE": "Put_LTP",
+        }
+    )
+
+    return expiry_list, final_df
 
 
 while True:
     try:
         data = oc(sym, exp_date)
-        sh1.clear()
+        # Clear sheet with every interval
+        # sh1.clear()
+        column_range.clear_contents()
         sh1.range("A1").value = data[1]
+
+        # Clear sheet with every interval
         sh2.clear()
         sh2.range("A1").options(transpose=True).value = data[0]
 
-        time.sleep(1)
+        total_call_change = sh1.range(call_oi_chg_column).value
+        total_put_change = sh1.range(put_oi_chg_column).value
+
+        pcr = round(total_put_change / total_call_change, 3)
+        print("PCR: ", pcr)
+
+        curr_time = time.strftime("%H:%M:%S", time.localtime())
+        sh1.range("J{}".format(next_row)).value = curr_time
+        sh1.range("K{}".format(next_row)).value = pcr
+
+        # Save the sheet
+        file.save()
+        # file.close()
+
+        time.sleep(interval)
+
+        next_row = next_row + 1
     except:
         print("Retrying")
-        time.sleep(3)
-
-
-# print(df.columns)
+        time.sleep(5)
